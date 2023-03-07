2881a9e3584d1267d5f6e51868055e0adfffefdb
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-03-07 20:14:40 -0300

    Agregue el ejercicio14 a la rama ejercicio14

---
+++ js/Ejercicio14.js
@@ -0,0 +1,13 @@
+let texto = prompt("Ingresar texto:");
+let longitud = texto.length;
+
+for(let i=0; i< longitud; i++){
+    let textoConGuion = texto.charAt(i);
+
+    if(i == longitud - 1){
+        document.write(textoConGuion);
+    }
+    else{
+        document.write(`${textoConGuion} - `);
+    }
+}
